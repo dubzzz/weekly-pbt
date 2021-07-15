72e56e00798dced232ea79dd2d5835744b8aace7
Nicolas DUBIEN <github@dubien.org>
2021-07-15 21:06:01 +0200

    Shift based footprints

--- 002-lastIndexOf.ts
+++ 002-lastIndexOf.ts
@@ -6,13 +6,13 @@ export function lastIndexOf(pattern: string, text: string): number {
   }
   let requestedFootprint = 0n;
   for (let index = pattern.length - 1; index >= 0; --index) {
-    requestedFootprint = requestedFootprint * 65536n + BigInt(pattern.charCodeAt(index));
+    requestedFootprint = (requestedFootprint << 16n) + BigInt(pattern.charCodeAt(index));
   }
   let movingFootprint = 0n;
   const startIndex = text.length - pattern.length;
-  const modSize = 65536n ** BigInt(pattern.length);
+  const modSize = 1n << (16n * BigInt(pattern.length));
   for (let index = text.length - 1; index >= 0; --index) {
-    movingFootprint = (movingFootprint * 65536n + BigInt(text.charCodeAt(index))) % modSize;
+    movingFootprint = ((movingFootprint << 16n) + BigInt(text.charCodeAt(index))) % modSize;
     if (index <= startIndex && movingFootprint === requestedFootprint) {
       return index;
     }
