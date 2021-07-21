ddf893bc8abcc4279c92926c92b14a1262a7d40c
Nicolas DUBIEN <github@dubien.org>
2021-07-22 01:41:13 +0200

    Rework racePodium

--- 013-racePodium.spec.ts
+++ 013-racePodium.spec.ts
@@ -1,16 +1,16 @@
 import fc from 'fast-check';
-import { racePodium } from './013-racePodium';
+import { RaceParticipants, racePodium } from './013-racePodium';
 
 describe('013-racePodium', () => {
   it('should predict the right podium', () => {
     fc.assert(
-      fc.property(tupleN(fc.nat(), 16), (speeds) => {
+      fc.property(tupleN(fc.nat(), 25), (speeds) => {
         // Arrange
         const compareParticipants = (pa: number, pb: number) => {
           if (speeds[pa] !== speeds[pb]) return speeds[pb] - speeds[pa];
           else return pa - pb;
         };
-        const runRace = (...participants: [number, number, number, number]): [number, number, number, number] => {
+        const runRace = (...participants: RaceParticipants): RaceParticipants => {
           return participants.sort(compareParticipants);
         };
 
@@ -18,9 +18,30 @@ describe('013-racePodium', () => {
         const podium = racePodium(runRace);
 
         // Assert
-        const rankedParticipants = [...Array(16)].map((_, i) => i).sort(compareParticipants);
+        const rankedParticipants = [...Array(25)].map((_, i) => i).sort(compareParticipants);
         const expectedPodium = rankedParticipants.slice(0, 3);
         expect(podium).toEqual(expectedPodium);
+      })
+    );
+  });
+
+  it('should never do more than 7 races', () => {
+    fc.assert(
+      fc.property(tupleN(fc.nat(), 25), (speeds) => {
+        // Arrange
+        const compareParticipants = (pa: number, pb: number) => {
+          if (speeds[pa] !== speeds[pb]) return speeds[pb] - speeds[pa];
+          else return pa - pb;
+        };
+        const runRace = jest.fn((...participants: RaceParticipants): RaceParticipants => {
+          return participants.sort(compareParticipants);
+        });
+
+        // Act
+        racePodium(runRace);
+
+        // Assert
+        expect(runRace.mock.calls.length).toBeLessThanOrEqual(7);
       })
     );
   });
@@ -30,7 +51,7 @@ describe('013-racePodium', () => {
 
 function tupleN<T>(arb: fc.Arbitrary<T>, n: number): fc.Arbitrary<T[]> {
   // In theory we may want to use something like:
-  //   fc.array(arb, {minLength: 16, maxLength: 16})
+  //   fc.array(arb, {minLength: n, maxLength: n})
   // as it results into a simpler code. Unfortunately, for the moment it does not shrink as well as tuples.
   return fc.tuple(...Array<fc.Arbitrary<T>>(n).fill(arb));
 }
--- 013-racePodium.ts
+++ 013-racePodium.ts
@@ -1,69 +1,58 @@
+export type RaceParticipants = [number, number, number, number, number];
+
 /**
- * Find the three fastest candidates among: [0, 1, 2, ..., 15].
- * In order to do so you are given a runRace function able to run a race with four participants.
- * You may consider that our candidates are really regular so they run with the exact same time and are never tired.
+ * 25 Horses Puzzle
+ * >  Let’s say that you have 25 horses, and you want to pick the fastest 3 horses out of those 25.
+ * >  In each race, only 5 horses can run at the same time because there are only 5 tracks.
+ * >  What is the minimum number of races required to find the 3 fastest horses without using a stopwatch?
+ *
+ * Find the podium!
+ * See https://matt-croak.medium.com/google-interview-25-horses-c982d0a9b3af for more details.
  *
  * @param runRace - Run a race with the received participants. Outputs the final ranking. In case of equality the participant with the smallest id wins.
  *
  * @returns
  * Ordered top three.
  */
-export function racePodium(
-  runRace: (...participants: [number, number, number, number]) => [number, number, number, number]
-): [number, number, number] {
-  const raceResults: [number, number, number, number][] = [];
+export function racePodium(runRace: (...participants: RaceParticipants) => RaceParticipants): [number, number, number] {
+  const raceResults: RaceParticipants[] = [];
 
   // We start with one run per candidate first
-  //   c0  c1  c2  c3  <-- race #1
-  //   c4  c5  c6  c7  <-- race #2
-  //   c8  c9 c10 c11  <-- race #3
-  //  c12 c13 c14 c15  <-- race #4
-  for (let index = 0; index !== 4; ++index) {
-    const ca = index * 4;
-    const cb = index * 4 + 1;
-    const cc = index * 4 + 2;
-    const cd = index * 4 + 3;
-    raceResults.push(runRace(ca, cb, cc, cd));
+  //   c0  c1  c2  c3  c4  <-- race #1
+  //   c5  c6  c7  c8  c9  <-- race #2
+  //  ...
+  for (let index = 0; index !== 5; ++index) {
+    const ca = index * 5;
+    const cb = index * 5 + 1;
+    const cc = index * 5 + 2;
+    const cd = index * 5 + 3;
+    const ce = index * 5 + 4;
+    raceResults.push(runRace(ca, cb, cc, cd, ce));
   }
 
   // Run a race against all the winners
-  raceResults.push(runRace(raceResults[0][0], raceResults[1][0], raceResults[2][0], raceResults[3][0]));
-  // But the second may never won a race and be second in the first race too (if winner was in the same race)
-  // So potential seconds are: all firsts on first races (except winner) + second of the race with winner
-  // But from race#5, we also know that the last of this last race will not be on podium.
-  // And we also learnt that the candidate that ranked second against the third of last race will not be on podium too.
-  const firstOfWinnerRace = raceResults[4][0];
-  const firstRaceOfFirstWinnerRace = Math.floor(firstOfWinnerRace / 4);
-  // The second is one of:
-  // - raceResults[firstRaceOfFirstWinnerRace][1] as it was second when it raced against winner
-  // - raceResults[4][1] as it was second when running against winner
-  // The third is one of:
-  // - raceResults[firstRaceOfFirstWinnerRace][1]
-  // - raceResults[firstRaceOfFirstWinnerRace][2]
-  // - raceResults[4][1] (second of winner race)
-  // - raceResults[index of first race for raceResults[4][1]][1]
-  // - raceResults[4][2] (third of winner race)
+  raceResults.push(
+    runRace(raceResults[0][0], raceResults[1][0], raceResults[2][0], raceResults[3][0], raceResults[4][0])
+  );
+  const winnersOriginalRaces = raceResults[5].map((p) => Math.floor(p / 5));
+
+  // Based on the race right before we know that the only eligible candidates for a second place are:
+  // - raceResults[5][1]
+  // - raceResults[winnersOriginalRaces[0]][1]
+  // For a third place are:
+  // - raceResults[5][2]
+  // - raceResults[winnersOriginalRaces[0]][2]
+  // - raceResults[winnersOriginalRaces[1]][1]
+  // + the one of the ones for second place
   raceResults.push(
     runRace(
-      raceResults[4][1],
-      raceResults[4][2],
-      raceResults[firstRaceOfFirstWinnerRace][1],
-      raceResults[firstRaceOfFirstWinnerRace][2]
+      raceResults[5][1],
+      raceResults[winnersOriginalRaces[0]][1],
+      raceResults[5][2],
+      raceResults[winnersOriginalRaces[0]][2],
+      raceResults[winnersOriginalRaces[1]][1]
     )
   );
-  const podiumSecond = raceResults[5][0];
-  if (podiumSecond === raceResults[firstRaceOfFirstWinnerRace][1]) {
-    // The third is the second of the last race
-    return [firstOfWinnerRace, podiumSecond, raceResults[5][1]];
-  }
-  // If second of winner race, we need the run another race as we don't know
-  // what would be the ranking for raceResults[firstRaceOfSecondWinnerRace][1].
-  // The two others participants of previous race cannot be first, not eligible for second rank.
-  const secondOfWinnerRace = raceResults[4][1];
-  const firstRaceOfSecondWinnerRace = Math.floor(secondOfWinnerRace / 4);
-  raceResults.push(
-    runRace(raceResults[5][1], raceResults[5][2], raceResults[5][3], raceResults[firstRaceOfSecondWinnerRace][1])
-  );
 
-  return [firstOfWinnerRace, podiumSecond, raceResults[6][0]];
+  return [raceResults[5][0], raceResults[6][0], raceResults[6][1]];
 }
